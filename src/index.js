d7e48e218d23406523fcfaa7fbb0df1deb8eaa54
Andy Lushman <andylushman@gmailcom>
2018-01-31 12:13:20 -0700

    Added VideoDetail and VideoList

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoList';
 const APIKey = 'AIzaSyBSRMoi4_1RfGXt1bVQXfxHqYXSz4HdrE4';
 
-//Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-//Take this componet's generated HTML and put it on the page (in the DOM)
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({ key: APIKey, term: 'Mountain Biking' }, videos => {
+      this.setState({ videos });
+      //this.setState({videos: videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
