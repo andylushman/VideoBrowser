8324ff6862bd84cbbe92a586ce1f70c9b8a5403a
Andy Lushman <andylushman@gmailcom>
2018-02-13 18:57:27 -0700

    Refactored into create-react-app boilerplate

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './Components/SearchBar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './Components/VideoList';
+import VideoDetail from './Components/VideoDetail';
+const APIKey = 'AIzaSyBSRMoi4_1RfGXt1bVQXfxHqYXSz4HdrE4';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('Mountain Biking');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: APIKey, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+      //this.setState({videos: videos});
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//Questions
+//1. Why two css files? Index.css and App.css..
+//2. How does npm start server on port 3000?
+//3. Why not always create a class insead of functional components and class components
--- src/index.js
+++ src/index.js
@@ -1,49 +1,8 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/SearchBar';
-import YTSearch from 'youtube-api-search';
-import VideoList from './components/VideoList';
-import VideoDetail from './components/VideoDetail';
-const APIKey = 'AIzaSyBSRMoi4_1RfGXt1bVQXfxHqYXSz4HdrE4';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    };
-
-    this.videoSearch('Mountain Biking');
-  }
-
-  videoSearch(term) {
-    YTSearch({ key: APIKey, term: term }, videos => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-      //this.setState({videos: videos});
-    });
-  }
-
-  render() {
-    const videoSearch = _.debounce(term => {
-      this.videoSearch(term);
-    }, 300);
-    return (
-      <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-          videos={this.state.videos}
-        />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
