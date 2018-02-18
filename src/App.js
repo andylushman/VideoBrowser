9c5b4283509ac1cc4dc80c02b301e1bb71361956
Andy Lushman <andylushman@gmail.com>
2018-02-17 18:19:35 -0700

    Added question in app.js

--- src/App.js
+++ src/App.js
@@ -52,3 +52,4 @@ export default App;
 //1. Why not always create a class component insead of functional components?
 //2. Why two css files? Index.css and App.css..
 //3. How does npm start server on port 3000?
+//4. Recommend any software for conceptualizing components/apps? Instead of Word.
